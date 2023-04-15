ca9d6fc6223ac50f91ce24dcbdf9e06a4707a623
coteensignia <mjensignia@gmail.com>
2023-04-14 23:59:53 -0400

    css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+
+//Tomar  del input
+let numberCard = document.getElementById("number-card");
+//tomar boton
+let btnValidator = document.getElementById("btn-validator");
+
+
+// haciendo click en el boton, que tome lo del input
+
+btnValidator.addEventListener("click", () => {
+    //convertimos en array, separamos y reversamos
+    let n = numberCard.value.split("").reverse();
+    //recorremos
+    n.forEach(element => {
+
+    });
+    //imprimir en consola
+    console.log(n);
+
+});
+
+
+
+
+
+
+//enmascarar
+
+// tomar mi inpud
+// let numberCard = document.getElementById("number-card");
+
+// tomar el boton
+
+// cuando haga click en boton tome el input
+// lo imprimirmos en pantalla

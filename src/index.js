524858d2a5873bf2dc4ca5806799263af438afd7
coteensignia <mjensignia@gmail.com>
2023-04-23 09:26:10 -0400

    primer test ok

--- src/index.js
+++ src/index.js
@@ -1,58 +1,19 @@
-//import validator from './validator.js';
+import validator from "./validator.js";
 
-//console.log(validator);
+const numberCard = document.getElementById("number-card"); //id numero input
+const btnValidator = document.getElementById("btn-validator"); // id boton
 
+//maximo minimo
+numberCard.addEventListener("input", () => {
+  //const num = numberCard.value;
+  // this.value = v.slice(0, 16);
+  //console.log(validator.maskify(numberCard.value));
+});
 
-//VALIDAR
-//Tomar  del input y boton
+//Is valid
 
-const numberCard = document.getElementById("number-card");
-const btnValidator = document.getElementById("btn-validator");
-
-
-
-btnValidator.addEventListener('click', () => {
-        let numberCardR = numberCard.value.toString().split("").reverse();
-        let resultadoFinal = ""
-
-        numberCardR.forEach((value, index) => {
-            if (index % 2 === 0) {
-                resultadoFinal += value
-            } else {
-                let multiplicacion = value * 2;
-
-                if (multiplicacion <= 9) {
-                    resultadoFinal += multiplicacion
-                } else {
-                    let nuevo = multiplicacion.toString().split("")
-                    let nuevo2 = nuevo.reduce((acumular, num_actual) => {
-                        return parseInt(acumular) + parseInt(num_actual)
-                    })
-                    resultadoFinal += nuevo2
-                }
-            }
-        });
-
-        let resultadoFinalSuma = resultadoFinal.split("").reduce((acum, num) => {
-            return parseInt(acum) + parseInt(num)
-        })
-        if (resultadoFinalSuma % 10 === 0) {
-            alert("numero valido " + resultadoFinalSuma)
-        } else {
-            alert("numero invalido " + resultadoFinalSuma)
-        }
-        console.log(resultadoFinalSuma)
-    }
-
-)
+btnValidator.addEventListener("click", () => {
+  validator.isValid(numberCard.value);
+});
 
 
-
-
-/*
-enmascarar
-tomar los numeros cuando el cliente escribe en el input
-
-
-
-*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
-//const validator = {
-// ...
-//};
+const validator = {
+  isValid: function (creditcardNumber) {
+    const numberCardR = creditcardNumber.split("").reverse();
+    let resultadoFinal = "";
 
-//export default validator;
+    numberCardR.forEach((value, index) => {
+      if (index % 2 === 0) {
+        resultadoFinal += value;
+      } else {
+        const multiplicacion = value * 2;
+
+        if (multiplicacion <= 9) {
+          resultadoFinal += multiplicacion;
+        } else {
+          const nuevo = multiplicacion.toString().split("");
+          resultadoFinal += parseInt(nuevo[0]) + parseInt(nuevo[1]);
+        }
+      }
+    });
+
+    let total = 0;
+    resultadoFinal = resultadoFinal.split("");
+
+    resultadoFinal.forEach((value) => {
+      total += parseInt(value);
+    });
+
+    if (total % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  maskify: function (creditcardNumber) {
+    return creditcardNumber;
+
+    /*
+                        const maskedNumber = creditcardNumber.slice(0, -4).replace(/./g, "#"); //metodo slice para tomar solo una parte del indice. /g es un modificador global (regular exp) se combina con replace 
+                        return maskedNumber + numberCard.slice(-4); //-4 porque es de atras para adelante
+
+
+                        console.log(m)
+                        return creditcardNumber
+                    }
+*/
+  },
+};
+
+export default validator;
